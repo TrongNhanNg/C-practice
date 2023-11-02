ac4f4bd10180c93e7ede55f05d3e166e03ec9c2b
TrongNhanNg <nguyentrongnhan0401000@gmail.com>
2023-11-02 14:47:18 +0700

    arr now have func find max

--- Arr1.c
+++ Arr1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-//ham nhap mang
+//Arr input
 void input( int arr[],int n)
 {
     
     for (int i = 0; i < n; i++)
-    {
+    {   
+        printf("Arr[%d] = ", i);
+        fflush(stdout);
         scanf("%d", &arr[i]);
     }
 }
 
-//ham xuat mang
+//Arr output
 void output(int arr[], int n)
 {  
     printf("Arr output: \n");
@@ -21,46 +23,62 @@ void output(int arr[], int n)
     printf("\n");
 }
 
-//ham tim kiem
-int find(int arr[], int n)
+//Search for X value in arr
+void find(int arr[], int n)
 {
     int x ;
-    printf("Nhap x: \n");
+    printf("Input X to search: \n");
     scanf("%d", &x);
+    
+    int ans = 0;
     for (int i = 0; i < n; i++)
     {
         if ( arr[i] == x)
         {
-            printf("x nam tai vi tri: arr[%d]\n", i);
-        }
-        else 
-        {
-            printf("x ko co trong arr\n");
+            printf("X at: arr[%d] \n", i );
+            ans = 1;
         }
     }
+    if( ans == 0)
+        printf("No X in arr \n");
     
 }
-
+// Find Max
+int max(int arr[], int n)
+{
+    int max = 0;
+    for ( int i = 0; i < n; i++)
+    {
+        if( arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
 int main() {
 
 int n1, n2;
 printf("Nhap n1:");
-scanf("%d", &n1);
+fflush(stdout);
+scanf("%u", &n1);
 int arr1[n1];
 input(arr1, n1);
 
 
-printf("Nhap n2:");
-scanf("%d", &n2);
-int arr2[n2];
-input(arr2, n2);
+// printf("Nhap n2:");
+// scanf("%d", &n2);
+// int arr2[n2];
+// input(arr2, n2);
 
 output(arr1,n1);
-output(arr2,n2);
+// output(arr2,n2);
 
 find(arr1,n1);
-find(arr2,n2);
+// find(arr2,n2);
+
+printf("Max of arr: %d \n", max(arr1, n1));
 
 return 0;
 }
